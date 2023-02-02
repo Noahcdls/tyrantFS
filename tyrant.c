3942f7e77b1bc0229df5f98dd47bf0e3659f0898
noahcdls <noahcdls@gmail.com>
2023-02-01 17:53:55 -0800

    Added basic mkfs

--- tyrant.c
+++ tyrant.c
@@ -6,6 +6,37 @@
 #include "tyrant.h"
 
 uint8_t * memspace;
+/*
+@brief mkfs for tyrant DEFAULT
+@param fs_space pointer to where beginning of memory space to write over
+*/
+void tfs_mkfs(void * fs_space){
+    for(int i = 1; i < END_OF_INODE; i++){
+        for(int j = 0; j < BLOCKSIZE; j+=128){
+            node * inode_init = fs_space + 4096*i + j;//set up a basic node
+            bzero(inode_init, 128);//clear out inode info
+        }
+    }
+    *((uint32_t *)fs_space) = END_OF_INODE;//first superblock
+    uint32_t* free_blockptr = fs_space + END_OF_INODE*BLOCKSIZE; //shift pointer 
+    uint32_t block_counter = END_OF_INODE;
+    for(int i = 0; i < 3; i++){//fill three blocks with addresses for fun
+        for(int j = 0; j < BLOCKSIZE/4; j++){//fill entire block
+            *free_blockptr = block_counter+1;//write in next free block
+            block_counter++;//increment next available block
+            if(j == BLOCKSIZE/4-1){//at end before reiterate
+                free_blockptr = block_counter*BLOCKSIZE;//jump to the next block
+                block_counter++;//additional increment since we used block counter to hold next free block
+            }
+            else{
+                free_blockptr++;//go to next 4 bytes in block
+            }
+        }
+    }
+
+
+}
+
 
 
 void tfs_mkdir(){
--- tyrant.h
+++ tyrant.h
@@ -9,23 +9,36 @@
 #define END_OF_INODE 3 //3 is first data block, 2 is last inode
 #define NUM_FREE_BLOCKS 64
 
+
 //ext4 stuff but worth copying some over
 //https://docs.kernel.org/filesystems/ext4/dynamic.html
 struct inode{
     uint32_t user_id;
     uint32_t group_id;
     uint32_t mode;//permissions, file type (dir, file, special)
-    uint32_t size_hi, size_lo;//size i bytes
-    uint32_t access_time, change_time, data_time;//time of last access, changes to inode(ex. name), and last data modification time
-    uint32_t creation_time, deletion_time;
+    uint32_t size_hi;
+    uint32_t size_lo;//size i bytes
+    uint32_t access_time;
+    uint32_t change_time;
+    uint32_t data_time;//time of last access, changes to inode(ex. name), and last data modification time
+    uint32_t creation_time;
+    uint32_t deletion_time;
     uint32_t links;//linked count
-    uint32_t blocks_hi, blocks_lo;//num of blocks
-    uint32_t direct_blocks[12], indirect_blocks, dbl_indirect, trpl_indirect;//holds block positions for blocks
+    uint32_t blocks_hi;
+    uint32_t blocks_lo;//num of blocks
+    uint32_t direct_blocks[12]
+    uin32_t indirect_blocks;
+    uint32_t  dbl_indirect;
+    uint32_t trpl_indirect;//holds block positions for blocks
     uint32_t flags;
     uint32_t checksum;
 
 
 };
 typedef struct inode node;
+ 
+#define DIRECTORY 0x4000
+#define FILE      0x8000
+
 
 
