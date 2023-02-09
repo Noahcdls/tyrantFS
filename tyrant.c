fd68ecf85fa085ce7abc065dea1388cb1ccd51cf
noahcdls <noahcdls@gmail.com>
2023-02-09 12:25:35 -0800

    working on mkdir. Added tyrant helper func file

--- tyrant.c
+++ tyrant.c
@@ -10,21 +10,40 @@ uint8_t *memspace;
 
 /*
 @brief make a directory path by allocating an inode and adding it to the directory list
+@param path pathname of new directory
+@param m Permissions passed by system, 32-bit
+@note TODO: Find parent path to determine where to add this file
 
 */
 int tfs_mkdir(const char * path, mode_t m)
 {
+
+
+
     node * dir_node = allocate_inode(memspace);
+    node * parent_node = NULL;//TODO: FIND PARENT NODE
     if(dir_node == NULL)
         return -1;
+    uint8_t * block = allocate_block(memspace);
+    if(block == NULL){
+        free_inode(dir_node);//free node if we cant make a full directory
+        return -1;
+    }
+
     //https://jameshfisher.com/2017/02/24/what-is-mode_t/
     //what mode_t means
     dir_node->mode = m;//definitions for mode can be found in sys/stat.h and bits/stat.h (bits for actual numerical value)
     dir_node->mode |= S_IFDIR;
+    write_block(".", block, 0, sizeof("."));
+    write_block(&dir_node, block, MAX_NAME_LENGTH, sizeof(dir_node));
+
+    write_block("..", block, PATH_BOUNDARY, sizeof(".."));
+    write_block(&parent_node, )
 
 
 
-    
+
+
     return 0;
 }
 void tfs_getattr()
--- tyrant.h
+++ tyrant.h
@@ -4,5 +4,9 @@
 #include <string.h>
 #include "disk.h"
 
+#define MAX_NAME_LENGTH 56 //maximum name length of a file or directory
+#define PATH_BOUNDARY 64    //name + address of inode
+
+
 
 
